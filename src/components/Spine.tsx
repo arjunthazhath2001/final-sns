0c8be9696e0c7287019d4e260acb523d3b2f6321
arjunthazath <arjunta32@gmail.com>
2024-10-12 11:43:44 +0530

    ok

--- src/components/Spine.tsx
+++ src/components/Spine.tsx
@@ -7,26 +7,28 @@ export default function Spine() {
     <div className='bg-transparent w-full min-h-screen flex flex-col items-center justify-center space-y-4 sm:space-y-8 relative'>
       {/* Title Section */}
       <div className="w-full text-center">
-        <span className="sm:text-3xl md:text-4xl text-xl lg:text-5xl xl:text-7xl font-extrabold text-yellow-400">SPINE :<span className='text-black dark:text-white'> Our Unique Approach</span> </span>
+        <span className="sm:text-3xl md:text-4xl text-xl lg:text-5xl xl:text-7xl font-extrabold text-yellow-400">
+          SPINE :
+          <span className='text-black dark:text-white'> Our Unique Approach</span>
+        </span>
+
+        {/* Small paragraph below title */}
+        <p className="text-sm sm:text-base lg:text-md text-gray-400 mt-5">
+          To provide an activity centre that will be user-friendly and open to all groups and members of the community.
+        </p>
       </div>
 
-
-
-
-    {/* CHANGE THE MAIN UI COMPONENT HERE */}
-
+      {/* Main UI Components */}
       <div className="w-full max-w-7xl px-4 md:px-0 grid sm:gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-        {/* First column of 5 cards */}
+        {/* First column of cards */}
         <div className="flex flex-col sm:gap-4">
-          <ExpandableCardDemo1/>
-          
+          <ExpandableCardDemo1 />
         </div>
-        {/* Second column of 5 cards */}
+        {/* Second column of cards */}
         <div className="flex flex-col sm:gap-4">
-          <ExpandableCardDemo2/>    
+          <ExpandableCardDemo2 />
         </div>
       </div>
-
-      </div>
-  )
+    </div>
+  );
 }
