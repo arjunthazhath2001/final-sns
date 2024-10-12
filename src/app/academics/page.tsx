da5637185de41a9a2bbd49375bfeaa023eb1cc6e
arjunthazath <arjunta32@gmail.com>
2024-10-12 11:32:36 +0530

    ok

--- src/app/academics/page.tsx
+++ src/app/academics/page.tsx
@@ -11,8 +11,8 @@ export default function AcademicsPage() {
   return (
     <div className="flex flex-col w-full">
       {/* Section 1 */}
-      <section className="pt-20 pb-44 flex items-center justify-center px-4 sm:px-6">
-        <div className="w-full xl:mt-20 text-center">       
+      <section className="pt-20 pb-44 xl:mb-20 flex items-center justify-center px-4 sm:px-6">
+        <div className="w-full text-center">       
           <SparklesPreview/>
         </div>
       </section>
