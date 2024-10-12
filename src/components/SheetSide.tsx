fd07ab25a8044b355d1f83b7ac67355ac5395687
arjunthazath <arjunta32@gmail.com>
2024-10-12 09:54:07 +0530

    ok

--- src/components/SheetSide.tsx
+++ src/components/SheetSide.tsx
@@ -28,7 +28,7 @@ export function SheetSide() {
           {/* SVG for hamburger icon */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 dark:text-white"
+            className="h-6 w-6 dark:text-white text-black"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
