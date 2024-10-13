5bd4ca6c6236d07971f18fca418dbeae17414aef
arjunthazath <arjunta32@gmail.com>
2024-10-13 19:59:35 +0530

    ok

--- src/app/academics/page.tsx
+++ src/app/academics/page.tsx
@@ -39,7 +39,7 @@ export default function AcademicsPage() {
       </section>
 
       {/* Section 5 */}
-      <section className="sm:max-h-[600px] md:my-24 xl:my-32 xl:mb-0 mb-20 flex items-center justify-center px-4 sm:px-6">
+      <section className="sm:max-h-[600px] md:my-24 xl:my-28 xl:mb-0 mb-20 flex items-center justify-center px-4 sm:px-6">
         <div className="w-full xl:mt-20 text-center">
           <Spine/>
         </div>
@@ -47,14 +47,14 @@ export default function AcademicsPage() {
       
 
       {/* Section 6 */}
-      <section className="sm:max-h-[600px] md:my-24 lg:my-32 xl:mb-0 mb-20 flex items-center justify-center px-4 sm:px-6">
+      <section className="sm:max-h-[600px] md:my-24 lg:my-28 xl:mb-0 mb-20 flex items-center justify-center px-4 sm:px-6">
         <div className="w-full xl:mt-52 text-center">
           <OurTech/>
         </div>
       </section>
 
       {/* Section 7 */}
-      <section className="sm:max-h-[1000px] md:my-24 lg:my-32 xl:mb-0 mb-20 flex items-center justify-center px-4 sm:px-6">
+      <section className="sm:max-h-[1000px] md:my-24 lg:my-28 xl:mb-0 mb-20 flex items-center justify-center px-4 sm:px-6">
         <div className="w-full xl:mt-2 text-center">
           <FaqAcademics/>
         </div>
