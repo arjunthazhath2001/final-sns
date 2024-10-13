aadaed8f5ae308dfb1afd95fe638875442d6fac8
arjunthazath <arjunta32@gmail.com>
2024-10-13 19:45:00 +0530

    ok

--- src/app/academics/page.tsx
+++ src/app/academics/page.tsx
@@ -18,28 +18,28 @@ export default function AcademicsPage() {
       </section>
 
       {/* Section 2 */}
-      <section className="sm:max-h-[600px] md:my-24 lg:my-32 xl:mt-32 xl:mb-0 mb-8 flex items-center justify-center px-4 sm:px-6">
+      <section className="sm:max-h-[600px] md:my-24 lg:my-32 xl:mt-32 xl:mb-0 mb-14 flex items-center justify-center px-4 sm:px-6">
         <div className="w-full text-center">
           <FeaturedPrograms/>
         </div>
       </section>
 
       {/* Section 3 */}
-      <section className="sm:max-h-[600px] md:my-24 lg:my-32 xl:mb-0 mb-8 flex items-center justify-center px-4 sm:px-6">
+      <section className="sm:max-h-[600px] md:my-24 lg:my-32 xl:mb-0 mb-14 flex items-center justify-center px-4 sm:px-6">
         <div className="w-full text-center">
           <OurFacilities/>
         </div>
       </section>
 
       {/* Section 4 */}
-      <section className="sm:max-h-[600px] md:my-24 lg:my-32 xl:mb-0 mb-8 flex items-center justify-center px-4 sm:px-6">
+      <section className="sm:max-h-[600px] md:my-24 lg:my-32 xl:mb-0 mb-14 flex items-center justify-center px-4 sm:px-6">
         <div className="w-full text-center">
           <Institutions/>
         </div>
       </section>
 
       {/* Section 5 */}
-      <section className="sm:max-h-[600px] md:my-24 lg:my-32 xl:mb-0 mb-14 flex items-center justify-center px-4 sm:px-6">
+      <section className="sm:max-h-[600px] md:my-24 lg:my-32 xl:mb-0 mb-20 flex items-center justify-center px-4 sm:px-6">
         <div className="w-full xl:mt-20 text-center">
           <Spine/>
         </div>
@@ -47,14 +47,14 @@ export default function AcademicsPage() {
       
 
       {/* Section 6 */}
-      <section className="sm:max-h-[600px] md:my-24 lg:my-32 xl:mb-0 mb-16 flex items-center justify-center px-4 sm:px-6">
+      <section className="sm:max-h-[600px] md:my-24 lg:my-32 xl:mb-0 mb-20 flex items-center justify-center px-4 sm:px-6">
         <div className="w-full xl:mt-52 text-center">
           <OurTech/>
         </div>
       </section>
 
       {/* Section 7 */}
-      <section className="sm:max-h-[1000px] md:my-24 lg:my-32 xl:mb-0 mb-16 flex items-center justify-center px-4 sm:px-6">
+      <section className="sm:max-h-[1000px] md:my-24 lg:my-32 xl:mb-0 mb-20 flex items-center justify-center px-4 sm:px-6">
         <div className="w-full xl:mt-2 text-center">
           <FaqAcademics/>
         </div>
