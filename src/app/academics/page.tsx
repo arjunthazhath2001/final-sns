f4083e3ff02c78bf15d821a7a115501a775b34e9
arjunthazath <arjunta32@gmail.com>
2024-10-13 19:38:42 +0530

    ok

--- src/app/academics/page.tsx
+++ src/app/academics/page.tsx
@@ -54,7 +54,7 @@ export default function AcademicsPage() {
       </section>
 
       {/* Section 7 */}
-      <section className="sm:max-h-[1000px] md:my-24 lg:my-32 xl:mb-0 mb-8 flex items-center justify-center px-4 sm:px-6">
+      <section className="sm:max-h-[1000px] md:my-24 lg:my-32 xl:mb-0 mb-16 flex items-center justify-center px-4 sm:px-6">
         <div className="w-full xl:mt-2 text-center">
           <FaqAcademics/>
         </div>
