adc25dde2533ddd5ad5bf6a0abf1a3616d19f3e9
arjunthazath <arjunta32@gmail.com>
2024-10-13 07:28:13 +0530

    ok

--- src/components/Spine.tsx
+++ src/components/Spine.tsx
@@ -7,7 +7,7 @@ export default function Spine() {
     <div className="bg-transparent w-full min-h-[80vh] flex flex-col items-center justify-center space-y-4 pt-24 relative">
       {/* Title Section */}
       <div className="w-full text-center">
-        <span className="sm:text-3xl md:text-4xl text-xl lg:text-5xl xl:text-7xl font-extrabold text-yellow-400">
+        <span className="text-3xl md:text-4xl lg:text-5xl xl:text-7xl font-extrabold text-yellow-400">
           SPINE : <span className='text-black dark:text-white'> Our Unique Approach</span>
         </span>
 
